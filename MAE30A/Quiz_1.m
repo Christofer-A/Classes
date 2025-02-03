db1aa93802d8ab83162771dad14f2e070998c018
Christofer-A <cazadian@ucsd.edu>
2025-02-03 13:09:48 -0800

    trolled

---
+++ MAE30A/Quiz_1.m
@@ -0,0 +1,17 @@
+% syms LE1 LE2 Ls F1 F2 phi theta thetax thetay thetaz
+% syms F1
+Ls = 0.3; LE2 = 0.3; LE1 = 0.25; phi = 45.0; theta = 45.0; thetay = 125.0; thetaz = 45.0; thetax = rad2deg(acos(sqrt(1-cos(deg2rad(thetay))^2-cos(deg2rad(thetaz))^2))); F2 = 125.0; F1 = 500;
+thetax
+rca = [-LE1 Ls*cos(deg2rad(30)) Ls*sin(deg2rad(30))]
+rda = [-LE1 (Ls*cos(deg2rad(30)) + LE2) Ls*sin(deg2rad(30))]
+Force1 = [-F1*cos(deg2rad(phi))*sin(deg2rad(theta)) -F1*cos(deg2rad(phi))*cos(deg2rad(theta)) -F1*sin(deg2rad(phi))]
+Force2 = [-F2*cos(deg2rad(thetax)) F2*cos(deg2rad(thetay)) F2*cos(deg2rad(thetaz))]
+
+M1 = cross(rca,Force2)
+M2 = cross(rda,Force1)
+
+Moment = M1 + M2
+M = sqrt(Moment(1)^2 + Moment(2)^2 + Moment(3)^2)
+format short
+vpa(M)
+
