be810168463cc613506778f042174ea90fd75175
Christofer-A <cazadian@ucsd.edu>
2025-04-26 22:25:17 -0700

    troll

--- MAE30B/HW 3/Q3.m
+++ MAE30B/HW 3/Q3.m
@@ -1,2 +1,10 @@
-clear; syms vbfx vbfy vcf
-va = [4 0]; vb = [0 0]; vc = [0 0]; vaf = [0 -1.92]; vbf = [vbfx vbfy]; vcf = [vcf 0];
+clear; syms vbfx vbfy vcfx
+va = [4 0]; vam = norm(va); vb = [0 0]; vc = [0 0]; vaf = [0 -1.92]; vafm = norm(vaf); vbf = [vbfx vbfy]; vbfm = norm(vbf); vcf = [vcfx 0]; vcfm = norm(vcf); 
+
+eqn1 = va == vaf + vbf + vcf;
+eqn2 = vam^2 == vafm^2 + vbfm^2 + vcfm^2;
+
+vpa(eqn1)
+vpa(eqn2)
+
+G = solve([eqn1, eqn2],[vbfx,vbfy,vcfx])
