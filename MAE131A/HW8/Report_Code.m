c9e2f66c53b3e23665df4affc2be26588ce16f97
Christofer-A <cazadian@ucsd.edu>
2025-06-03 21:25:19 -0700

    Create Report_Code.m

---
+++ MAE131A/HW8/Report_Code.m
@@ -0,0 +1,15 @@
+P = 300; T = 0.35; L = 0.16; d = 0.018; t = 0.002;
+E = 70*10^9; G = 26*10^9; sigmay = 240*10^6; tauy = 140*10^6;
+ro = d/2; ri = ro - t; A = pi*(ro^2 - ri^2); J = pi/2*(ro^4 - ri^4); 
+
+sigma = P/A 
+tau = T*ro/J
+
+sigma1 = sigma/2 + sqrt((sigma/2)^2 + tau^2)
+sigma2 = sigma/2 - sqrt((sigma/2)^2 + tau^2)
+
+sigmav = sqrt(sigma1^2 + sigma2^2 - sigma1*sigma2)
+
+phi = T*L/(J*G)*(180/pi)
+
+sigmas = sigmay/sigmav
