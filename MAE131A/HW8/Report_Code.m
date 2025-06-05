15a621580e6d8aa47668580958373c3fc63aee49
Christofer-A <cazadian@ucsd.edu>
2025-06-04 18:18:14 -0700

    trolling

--- MAE131A/HW8/Report_Code.m
+++ MAE131A/HW8/Report_Code.m
@@ -1,5 +1,6 @@
 P = 300; T = 0.35; L = 0.16; d = 0.018; t = 0.002;
 E = 70*10^9; G = 26*10^9; sigmay = 240*10^6; tauy = 140*10^6;
+E1 = 113*10^9; G1 = 44*10^9; sigmay1 = 880*10^6; tauy1 = 550*10^6;
 ro = d/2; ri = ro - t; A = pi*(ro^2 - ri^2); J = pi/2*(ro^4 - ri^4); 
 
 sigma = P/A 
@@ -12,4 +13,10 @@ sigmav = sqrt(sigma1^2 + sigma2^2 - sigma1*sigma2)
 
 phi = T*L/(J*G)*(180/pi)
 
-sigmas = sigmay/sigmav
+phi1 = T*L/(J*G1)*(180/pi)
+
+FS = sigmay/sigmav
+
+FS1 = sigmay1/sigmav
+
+1/sqrt(3)
