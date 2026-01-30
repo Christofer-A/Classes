b94063b0633de9a14c62fa15ad2a1e676dfdeab8
Christofer-A <cazadian@ucsd.edu>
2026-01-29 22:05:04 -0800

    trolling

--- MAE 125/D1.m
+++ MAE 125/D1.m
@@ -1,9 +1,13 @@
 clear all; close all;
-% T = readtable("One Day.csv")
-% plot(T.Var1, T.Var2) 
+T = readtable("One Day.csv")
+plot(T.Var1, T.Var3) 
 
 % T = readtable("One Week.csv")
-% plot(T.Var1, T.Var2) 
+% plot(T.Var1, T.Var3) 
 
-T = readtable("One Year.csv")
-plot(T.Var1, T.Var2) 
+% T = readtable("One Year.csv")
+% plot(T.Var1, T.Var3)
+
+
+ylabel('Energy (kWh)');
+xlabel('Time and Date');
