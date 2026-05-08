cea31c75b0c920802deea799d39f4d47b0b064ff
Christofer-A <cazadian@ucsd.edu>
2026-05-08 16:13:59 -0700

    troll

---
+++ MAE 170/Lab 5/MAE101B.m
@@ -0,0 +1,7 @@
+clear all; close all; clc;
+
+plot([0 0.001806 0.005725 0.008115 0.008601, 0.008633], 0:0.3:1.5);
+
+title('Nondimensional Y Velocity vs Nondimensional Boundary Layer Height');
+xlabel('v/u');
+ylabel('y/d');
