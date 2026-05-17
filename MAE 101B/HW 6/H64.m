0120bd31ea556d0f444d77feff712c810fc21db9
Christofer-A <cazadian@ucsd.edu>
2026-05-16 20:45:19 -0700

    troll

--- MAE 101B/HW 6/H64.m
+++ MAE 101B/HW 6/H64.m
@@ -85,3 +85,48 @@ hTable.Properties.RowNames = {'30 mph', '60 mph', '80 mph'};
 disp('Hyundai Performance Data')
 disp(hTable)
 
+Tesla = struct;
+
+tPd1 = Pd(mph1, 0.23, 2.65)/1000;
+tPd2 = Pd(mph2, 0.23, 2.65)/1000;
+tPd3 = Pd(mph3, 0.23, 2.65)/1000;
+
+tPr1 = Pr(mph1, 1990)/1000;
+tPr2 = Pr(mph2, 1990)/1000;
+tPr3 = Pr(mph3, 1990)/1000;
+
+tP1 = tPd1 + tPr1;
+tP2 = tPd2 + tPr2;
+tP3 = tPd3 + tPr3;
+
+Tesla.powerDrag_kW(1) = tPd1;
+Tesla.powerDrag_kW(2) = tPd2;
+Tesla.powerDrag_kW(3) = tPd3;
+
+Tesla.powerRollingResistance_kW(1) = tPr1;
+Tesla.powerRollingResistance_kW(2) = tPr2;
+Tesla.powerRollingResistance_kW(3) = tPr3;
+
+Tesla.totalPower_kW(1) = tP1;
+Tesla.totalPower_kW(2) = tP2;
+Tesla.totalPower_kW(3) = tP3;
+
+Tesla.energyConsumption_kWh_mi(1) = energyConsumption(tP1, 30);
+Tesla.energyConsumption_kWh_mi(2) = energyConsumption(tP2, 60);
+Tesla.energyConsumption_kWh_mi(3) = energyConsumption(tP3, 80);
+
+Tesla.range_mi(1) = range(75, Tesla.energyConsumption_kWh_mi(1));
+Tesla.range_mi(2) = range(75, Tesla.energyConsumption_kWh_mi(2));
+Tesla.range_mi(3) = range(75, Tesla.energyConsumption_kWh_mi(3));
+
+Tesla.powerDrag_kW = Tesla.powerDrag_kW';
+Tesla.powerRollingResistance_kW = Tesla.powerRollingResistance_kW';
+Tesla.totalPower_kW = Tesla.totalPower_kW';
+Tesla.energyConsumption_kWh_mi = Tesla.energyConsumption_kWh_mi';
+Tesla.range_mi = Tesla.range_mi';
+
+tTable = struct2table(Tesla);
+tTable.Properties.RowNames = {'30 mph', '60 mph', '80 mph'};
+
+disp('Tesla Performance Data')
+disp(tTable)
